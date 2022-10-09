4788e591506a815bd3a232d5f6690f1c8f1252c8
j450nx <35202241+j450nx@users.noreply.github.com>
2022-10-09 12:31:59 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,48 @@
+function isPalidrome (str) {
+    let length = Math.floor(str.length / 2);
+    
+    for (let i = 0; i < length; i++) {
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false;
+        }        
+    }
+    return true;
+}
+console.log(isPalidrome('1234444=5321'));
+
+
+function binaryGap(num) {
+    var binStr = num.toString(2),
+        currentMax,
+        finalMax = 0;
+    
+    for (let i = 0; i < binStr.length; i++) {
+        currentMax = 0;
+        while (binStr[i] === '0') {
+            ++currentMax && ++i;
+        }
+        finalMax = Math.max(currentMax, finalMax);
+    }
+    return finalMax;
+}
+
+console.log(`Largest binary gap: ${binaryGap(10141)}`);
+
+function smallestPositiveInteger(array) {
+    let resultArray = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] >= 0) {
+            resultArray[array[i]] = true;
+        }
+    }
+    
+    for (let i = 1; i <= resultArray.length; i++) {
+        if (resultArray[i] === undefined) {
+            return i;
+        }
+    }
+    return 1;
+}
+
+console.log(smallestPositiveInteger([1, -3, 6, 4, 1, 2]));
