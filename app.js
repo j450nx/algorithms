6b7b201e1414415d3d0f50c55da2889bc191a1ae
Jason <j450nx@gmail.com>
2022-10-27 17:10:16 -0400

    another practice with solution

--- app.js
+++ app.js
@@ -534,3 +534,19 @@ function mutation(arr) {
 console.log(mutation(["hello", "hey"]));
 console.log(mutation(["Alien", "line"]));
 console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]));
+
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/chunky-monkey
+/*
+Chunky Monkey
+Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+*/
+function chunkArrayInGroups(arr, size) {
+    let result = [];
+    for (let i = 0; i < arr.length; i+=size) {
+        result.push(arr.slice(i, i + size));
+    }
+    return result;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
