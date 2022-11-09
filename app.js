a3c2417986ee8914c21c08bf7df31a5b339f2b30
Jason <j450nx@gmail.com>
2022-11-09 11:47:09 -0500

    practice

--- app.js
+++ app.js
@@ -745,3 +745,28 @@ function translatePigLatin(str) {
   }
   
 console.log(translatePigLatin("cconsonant"));
+
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/search-and-replace
+/*
+Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+
+First argument is the sentence to perform the search and replace on.
+
+Second argument is the word that you will be replacing (before).
+
+Third argument is what you will be replacing the second argument with (after).
+
+Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word Book with the word dog, it should be replaced as Dog
+*/
+
+function myReplace(str, before, after) {
+    const index = str.indexOf(before);
+    if (str[index] === str[index].toUpperCase()) {
+        after = after.charAt(0).toUpperCase() + after.slice(1);
+    } else {
+        after = after.charAt(0).toLowerCase() + after.slice(1);
+    }
+    return str.replace(before, after);
+}
+console.log(myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped"));
+console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "Leaped"));
